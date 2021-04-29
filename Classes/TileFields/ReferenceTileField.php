62f075afddb19436b215ed1bf43d7270448bc1dd
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2021-04-29 10:46:06 +0200

    Add ReferenceTileField

---
+++ Classes/TileFields/ReferenceTileField.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package    con4gis
+ * @version    7
+ * @author     con4gis contributors (see "authors.txt")
+ * @license    LGPL-3.0-or-later
+ * @copyright  Küstenschmiede GmbH Software & Design
+ * @link       https://www.con4gis.org
+ */
+namespace con4gis\FrameworkBundle\Classes\TileFields;
+
+class ReferenceTileField extends TileField
+{
+    private $references = [];
+
+    const TYPE = 'reference';
+
+    public function getConfiguration() : array
+    {
+        $config = parent::getConfiguration();
+        $config['references'] = $this->references;
+
+        return $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function getReferences(): array
+    {
+        return $this->references;
+    }
+
+    /**
+     * @param array $references
+     */
+    public function setReferences(array $references): void
+    {
+        $this->references = $references;
+    }
+
+}

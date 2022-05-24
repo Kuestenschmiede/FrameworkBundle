b6f62b18c985437d3e9e76adc2abc415afea2f16
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-05-24 12:52:04 +0200

    Fix condition handling in Condition.jsx

--- src/Resources/public/js/condition/Condition.jsx
+++ src/Resources/public/js/condition/Condition.jsx
@@ -16,12 +16,36 @@ export default class Condition extends Component {
     super(props);
 
     this.isConditionMet = this.isConditionMet.bind(this);
+
+    let ok = true;
+    let conditions = [];
+
+    if (this.props.conditions && typeof this.props.conditions.forEach === 'function') {
+      try {
+        this.props.conditions.forEach(function(condition) {
+          if (this.isConditionMet(condition) === false) {
+            ok = false;
+            condition.ok = false;
+          } else {
+            condition.ok = true;
+          }
+          conditions.push(condition);
+        }, this);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    this.state = {
+      conditions: conditions,
+      ok: ok
+    };
   }
 
   isConditionMet(condition){
     let fieldValue, conditionValue;
     if (typeof this.props.data[condition.field] === 'object') {
-      if (typeof this.props.data[condition.field].value !== 'undefined') {
+      if (this.props.data[condition.field] !== null && typeof this.props.data[condition.field].value !== 'undefined') {
         fieldValue = String(this.props.data[condition.field].value);
       } else {
         fieldValue = null;
@@ -54,17 +78,7 @@ export default class Condition extends Component {
   }
 
   render() {
-    let render = true;
-
-    try {
-      this.props.conditions.forEach(function(condition) {
-        if (this.isConditionMet(condition) === false) {
-          render = false;
-        }
-      }, this);
-    } catch (e) {}
-    
-    if (render) {
+    if (this.state.ok) {
       return this.props.children;
     } else {
       return null;

476a9e8e161d6edb3fa175cd74a76c29c9111ab1
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2026-01-03 12:45:03 +0100

    Renew copyright hints

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,25 +2,21 @@
 /*
  * This file is part of con4gis, the gis-kit for Contao CMS.
  * @package con4gis
- * @version 10
- * @author con4gis contributors (see "authors.txt")
+ * @author con4gis contributors (see "authors.md")
  * @license LGPL-3.0-or-later
- * @copyright (c) 2010-2025, by Küstenschmiede GmbH Software & Design
+ * @copyright (c) 2010-2026, by Küstenschmiede GmbH Software & Design
  * @link https://www.con4gis.org
  */
 
 /**
  * con4gis for Contao Open Source CMS
  *
- * @version   php 7
  * @package   con4gis-Local (LocalBundle)
  * @author    con4gis contributors
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
- * @copyright Küstenschmiede GmbH Software & Design 2011 - 2019
  * @link      https://www.kuestenschmiede.de
  */
 
 use con4gis\CoreBundle\Classes\Helper\ArrayHelper;
 
 $GLOBALS['con4gis']['framework']['installed'] = true;
-
